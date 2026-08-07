06cdcf4bc316c433ad2bb343c537ba6fc6912889
Narik ... <kiranzafar320@gmail.com>
2026-08-07 21:54:04 +0500

    cryptoWallet.js

--- cryptoWallet.js
+++ cryptoWallet.js
@@ -1,1 +1,26 @@
+function createCryptoWallet(initialBalance){
+    let balance=initialBalance;
+    return{
+        deposit(amount){
+            balance += amount;
+        },
+        withdraw(amount){
+            if(amount>balance){
+                return "Transaction Failed: Insufficient funds.";
+            }
+            else{
+                balance -= amount;
+                return balance;
+            }
+        },
+        walletbalance(){
+            return balance;
+        }
+    };
+}
 
+let wallet=createCryptoWallet(2.5);
+wallet.deposit(1.2);
+console.log(wallet.walletbalance());
+console.log(wallet.withdraw(5));
+console.log(wallet.withdraw(1.5));
